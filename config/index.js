af22e1bb691a14e1785ac0d419f8a1a4666a0c6a
dxkshuai <112925994+dxkshuai@users.noreply.github.com>
2022-09-07 09:59:29 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -35,7 +35,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "opYJU6pByD6oNFwwbTW2ijmdb814",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "RKuFXNuN9yXFYy_WIs2mJXJZLRApqteNs3BRkuS0jKU",
+      useTemplateId: "caj0V2VauW4uqqIbOL0hUhoo8S6Dk3cnKiMvuHmsNec",
       // 所在省份
       province: "山西",
       // 所在城市
